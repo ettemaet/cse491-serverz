bc550b06d902a7f70de9b42b957dc79c0182a241
Ethan Ettema <ettemaet@arctic.cse.msu.edu>
2014-01-16 16:07:09 -0500

    review of hw1

--- server.py
+++ server.py
@@ -15,10 +15,13 @@ s.listen(5)                 # Now wait for client connection.
 
 print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
-    # Establish connection with client.    
+    # Establish connection with client.
     c, (client_host, client_port) = s.accept()
     print c.recv(1000)
     c.send('HTTP/1.0 200 OK\r\n')
     c.send("Content-Type: text/html\r\n\r\n")
+    # There are two apostrophes which ends a string and starts a new one.
+    # Should be escaped or use quotations.
+    # Line is longer than 80 characters.
     c.send('<html><body><h1>Hello, world</h1> this is aliomar''s Web server</body></html>')
     c.close()
